d764e5e45d3cb167609a8a912fd50852f3ef9db8
Tilwani <ltilwani@amd.com>
2024-04-21 22:56:07 +0530

    Get IP from env

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://localhost:2000/goals');
+        const response = await fetch(`http://${process.env.NODE_API}:2000/goals`);
 
         const resData = await response.json();
 
@@ -39,7 +39,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:2000/goals', {
+      const response = await fetch(`http://${process.env.NODE_API}:2000/goals`, {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -78,7 +78,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:2000/goals/' + goalId, {
+      const response = await fetch(`http://${process.env.NODE_API}:2000/goals/` + goalId, {
         method: 'DELETE',
       });
 
